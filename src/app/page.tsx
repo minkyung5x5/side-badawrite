914553d9a6e8d62aa727c3b66a313ca14e6f90cf
minkyung5x5 <minkyung5x5@gmail.com>
2024-06-28 22:12:00 +0900

    add Fullscreen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,86 @@
 "use client"
 
 import Image from "next/image";
-import { mic, micOff, scan, removeCircleOutline, addCircleOutline } from 'ionicons/icons';
+import { mic, micOff, scan, removeCircleOutline, addCircleOutline, exitOutline } from 'ionicons/icons';
 import { IonButton, IonIcon } from '@ionic/react';
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [isFullscreen, setIsFullscreen] = useState(false);
+  useEffect(() => {
+    const handleFullscreenChange = () => {
+      setIsFullscreen(
+        document.fullscreenElement ||
+        document.webkitFullscreenElement ||
+        document.mozFullScreenElement ||
+        document.msFullscreenElement
+      );
+    };
+
+    document.addEventListener('fullscreenchange', handleFullscreenChange);
+    document.addEventListener('webkitfullscreenchange', handleFullscreenChange);
+    document.addEventListener('mozfullscreenchange', handleFullscreenChange);
+    document.addEventListener('MSFullscreenChange', handleFullscreenChange);
+
+    return () => {
+      document.removeEventListener('fullscreenchange', handleFullscreenChange);
+      document.removeEventListener('webkitfullscreenchange', handleFullscreenChange);
+      document.removeEventListener('mozfullscreenchange', handleFullscreenChange);
+      document.removeEventListener('MSFullscreenChange', handleFullscreenChange);
+    };
+  }, []);
+
+  const handleFullscreen = () => {
+    if (!isFullscreen) {
+      enterFullscreen();
+    } else {
+      exitFullscreen();
+    }
+    setIsFullscreen(!isFullscreen); // Toggle fullscreen state
+  };
+
+  const enterFullscreen = () => {
+    const element = document.documentElement;
+    if (element.requestFullscreen) {
+      element.requestFullscreen();
+    } else if (element.webkitRequestFullscreen) { // Safari
+      element.webkitRequestFullscreen();
+    } else if (element.mozRequestFullScreen) { // Firefox
+      element.mozRequestFullScreen();
+    } else if (element.msRequestFullscreen) { // IE/Edge
+      element.msRequestFullscreen();
+    }
+  };
+
+  const exitFullscreen = () => {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { // Safari
+      document.webkitExitFullscreen();
+    } else if (document.mozCancelFullScreen) { // Firefox
+      document.mozCancelFullScreen();
+    } else if (document.msExitFullscreen) { // IE/Edge
+      document.msExitFullscreen();
+    }
+  };
+
   return (
-    <main className="">
+    <main>
       {/* <div className="text-emerald-500">{'Badawrite'}</div> */}
       <div className="relative h-screen bg-emerald-100">
         <div className="fixed bottom-0 left-0 w-full h-20 bg-emerald-500 opacity-50">
           <div className="w-full h-20 p-4 flex justify-between items-center bg-emerald-600">
             <div className="flex justify-between items-center space-x-2">
-              <IonIcon className="w-10 h-10" icon={removeCircleOutline} style={{ color: 'white' }}></IonIcon>
+              <IonIcon className="w-10 h-10 text-white hover:cursor-pointer" icon={removeCircleOutline} />
               <div className="text-lg text-white">{"16"}</div>
-              <IonIcon className="w-10 h-10" icon={addCircleOutline} style={{ color: 'white' }}></IonIcon>
+              <IonIcon className="w-10 h-10 text-white hover:cursor-pointer" icon={addCircleOutline} />
             </div>
-            <IonIcon className="w-10 h-10 p-2 rounded-full outline outline-2" icon={mic} style={{ color: 'white' }}></IonIcon>
-            <IonIcon className="w-10 h-10 " icon={scan} style={{ color: 'white' }}></IonIcon>
+            <IonIcon className="w-10 h-10 p-2 rounded-full outline outline-2 hover:cursor-pointer text-white" icon={mic} />
+            <IonIcon
+              onClick={handleFullscreen}
+              className="w-10 h-10 hover:cursor-pointer text-white"
+              icon={isFullscreen ? exitOutline : scan}
+            />
             {/* <IonIcon icon={micOff} style={{ fontSize: 32, color: 'white' }}></IonIcon> */}
           </div>
         </div>
